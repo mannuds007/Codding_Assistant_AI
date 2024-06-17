3f70a852b067f76ed609ffb536c8c0c4a09dac48
mannuds007 <123095135+mannuds007@users.noreply.github.com>
2024-06-17 14:38:22 +0530

    Update mainf.py

--- mainf.py
+++ mainf.py
@@ -24,7 +24,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
 ]
 
-model = genai.GenerativeModel(model_name="gemini-1.5-flash"
+model = genai.GenerativeModel(model_name="gemini-1.5-flash",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
